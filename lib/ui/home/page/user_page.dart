fc1fdbf2b2762aaf9c2437d067f713c3a5c0a0b1
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-07-28 23:44:52 +0100

    ajout de la  tamplate du page user

---
+++ lib/ui/home/page/user_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:tn_trips/common/constants.dart';
+
+class UserPage extends StatelessWidget {
+  final String name;
+  final String urlImage;
+
+  const UserPage({
+    Key? key,
+    required this.name,
+    required this.urlImage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.person),
+            )
+          ],
+          backgroundColor: kPrimaryColor,
+          title: Text(name),
+          centerTitle: true,
+        ),
+      );
+}
