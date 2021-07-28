3d9885ed198a56ca9f5c0de0f6b276ed5b2fa0e8
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-07-28 23:48:24 +0100

    ajout de la  tamplate du page location

---
+++ lib/ui/home/page/location_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tn_trips/common/constants.dart';
+class LocationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.location_on),
+            )
+          ],
+          title: Text('Location'),
+          centerTitle: true,
+          backgroundColor: kPrimaryColor,
+        ),
+      );
+}
