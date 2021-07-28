ce39091402693ac2be908344038d7ab1a1d36bee
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-07-28 23:47:14 +0100

    ajout de la  tamplate du page favourites

---
+++ lib/ui/home/page/favourites_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tn_trips/common/constants.dart';
+class FavouritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.favorite_border),
+            )
+          ],
+          title: Text('Favourites'),
+          centerTitle: true,
+          backgroundColor: kPrimaryColor,
+        ),
+      );
+}
