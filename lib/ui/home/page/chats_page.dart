52eb423258b9042b1e800c67565a07c5e551458f
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-07-29 14:14:20 +0100

    ajout du template de la page chats

---
+++ lib/ui/home/page/chats_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tn_trips/common/constants.dart';
+class ChatsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.chat),
+            )
+          ],
+          title: Text('Chats'),
+          centerTitle: true,
+          backgroundColor: kPrimaryColor,
+        ),
+      );
+}
