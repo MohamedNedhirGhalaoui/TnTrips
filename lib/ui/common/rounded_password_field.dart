622320034e359e38f16deb47da1f507cef1c6f15
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-07-29 22:14:24 +0100

    configiration du button visibiliter du password

--- lib/ui/common/rounded_password_field.dart
+++ lib/ui/common/rounded_password_field.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tn_trips/ui/common/text_field_container.dart';
 import 'package:tn_trips/common/constants.dart';
 
-class RoundedPasswordField extends StatelessWidget {
+class RoundedPasswordField extends StatefulWidget {
   final ValueChanged<String> onChanged;
   static String password = '';
   const RoundedPasswordField({
@@ -10,12 +10,19 @@ class RoundedPasswordField extends StatelessWidget {
   });
 
   @override
+  _RoundedPasswordFieldState createState() => _RoundedPasswordFieldState();
+}
+
+class _RoundedPasswordFieldState extends State<RoundedPasswordField> {
+  bool obscureText = true;
+  @override
   Widget build(BuildContext context) {
     return TextFieldContainer(
       child: TextFormField(
-        validator: (value) => value!.length < 6 ? "Enter an password 6 chars +" : null,
-        obscureText: true,
-        onChanged: onChanged,
+        validator: (value) =>
+            value!.length < 6 ? "Enter an password 6 chars +" : null,
+        obscureText: obscureText,
+        onChanged: widget.onChanged,
         cursorColor: kPrimaryColor,
         decoration: InputDecoration(
           hintText: "Password",
@@ -23,8 +30,13 @@ class RoundedPasswordField extends StatelessWidget {
             Icons.lock,
             color: kPrimaryColor,
           ),
-          suffixIcon: Icon(
-            Icons.visibility,
+          suffixIcon: IconButton(
+            icon: Icon(Icons.visibility),
+            onPressed: () {
+              setState(() {
+                obscureText = !obscureText;
+              });
+            },
             color: kPrimaryColor,
           ),
           border: InputBorder.none,
