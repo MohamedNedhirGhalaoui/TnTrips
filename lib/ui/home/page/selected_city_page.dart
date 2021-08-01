22e9275dea505e7d33ce08a23f0d13f4128e25f5
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-08-01 23:20:38 +0100

    update oll file of project

---
+++ lib/ui/home/page/selected_city_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SelectedCityPage extends StatelessWidget {
+  const SelectedCityPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("SELECTED CITY PAGE"),
+      ),
+    );
+  }
+}
