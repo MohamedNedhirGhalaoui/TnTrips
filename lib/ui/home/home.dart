90ce04ea091fd2f2f66889c895f73693357ab4f2
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-08-01 19:26:32 +0100

    update du fichier de projet

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -3,16 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:tn_trips/data/models/city.dart';
 import 'package:tn_trips/data/services/Utils.dart';
 import 'package:tn_trips/ui/home/drawer/navigation_drawer_widget.dart';
-import 'package:tn_trips/ui/home/page/chats_page.dart';
-import 'package:tn_trips/ui/home/page/contact_us_page.dart';
-import 'package:tn_trips/ui/home/page/location_page.dart';
-import 'package:tn_trips/ui/home/page/recommended_places_page.dart';
-import 'package:tn_trips/ui/home/page/settings_page.dart';
-import 'package:tn_trips/ui/home/page/user_page.dart';
+import 'package:tn_trips/ui/home/widget/build-bottom_navigation_bar.dart';
+import 'package:tn_trips/ui/home/widget/build_cityIcons.dart';
 
 class Home extends StatelessWidget {
-  final urlImage =
-      "https://scontent.ftun10-1.fna.fbcdn.net/v/t1.6435-9/198764751_1634485790073693_2079059947597904212_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=3qxqOM63EagAX9OzeaX&tn=mJOlMso_Q5NB-1Io&_nc_ht=scontent.ftun10-1.fna&oh=2a31ffd747f1da35a21f932364453dc5&oe=6127D881";
   List<City> citys = Utils.getCitys();
 
   @override
@@ -35,7 +29,7 @@ class Home extends StatelessWidget {
         elevation: 0.0,
       ),
       drawer: NavigationDrawerWidget(),
-      bottomNavigationBar: buildBottomNavigationBar(context),
+      bottomNavigationBar: BuildBottomNavigationBar(),
       body: Container(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -101,19 +95,19 @@ class Home extends StatelessWidget {
                                 SizedBox(
                                   width: 2.0,
                                 ),
-                                buildCityIcons(HOTELS, 0),
+                                BuildCityIcons(color: HOTELS, index: 0),
                                 SizedBox(
                                   width: 10.0,
                                 ),
-                                buildCityIcons(RESTAURANTS, 1),
+                                BuildCityIcons(color: RESTAURANTS, index: 1),
                                 SizedBox(
                                   width: 10.0,
                                 ),
-                                buildCityIcons(CAFES, 2),
+                                BuildCityIcons(color: CAFES, index: 2),
                                 SizedBox(
                                   width: 10.0,
                                 ),
-                                buildCityIcons(SPORTS, 3),
+                                BuildCityIcons(color: SPORTS, index: 3),
                                 SizedBox(
                                   width: 10.0,
                                 ),
@@ -131,95 +125,6 @@ class Home extends StatelessWidget {
                   }),
             )
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildBottomNavigationBar(BuildContext context) {
-    return Container(
-      height: 60,
-      width: double.infinity,
-      padding: EdgeInsets.all(10),
-      color: kPrimaryColor,
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 10),
-        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          IconButton(
-            splashColor: kPrimaryColor,
-            onPressed: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => LocationPage()));
-            },
-            icon: Icon(Icons.location_on),
-            color: Colors.white,
-            iconSize: 30.0,
-          ),
-          IconButton(
-            splashColor: kPrimaryColor,
-            onPressed: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => ChatsPage()));
-            },
-            icon: Icon(Icons.chat),
-            color: Colors.white,
-            iconSize: 30.0,
-          ),
-          IconButton(
-            splashColor: kPrimaryColor,
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => RecommendedPlacesPage()));
-            },
-            icon: Icon(Icons.recommend_rounded),
-            iconSize: 30.0,
-            color: Colors.white,
-          ),
-          IconButton(
-            splashColor: kPrimaryColor,
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) =>
-                      UserPage(name: "Nedhir", urlImage: urlImage)));
-            },
-            icon: Icon(Icons.person),
-            color: Colors.white,
-            iconSize: 30.0,
-          ),
-          IconButton(
-            splashColor: kPrimaryColor,
-            onPressed: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => ContactUsPage()));
-            },
-            icon: Icon(Icons.contact_mail),
-            color: Colors.white,
-            iconSize: 30.0,
-          ),
-          IconButton(
-            splashColor: kPrimaryColor,
-            onPressed: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => SettingsPage()));
-            },
-            icon: Icon(Icons.settings),
-            color: Colors.white,
-            iconSize: 30.0,
-          ),
-        ]),
-      ),
-    );
-  }
-
-  Widget buildCityIcons(Color color, int index) {
-    return ClipOval(
-      child: Container(
-        color: color,
-        padding: EdgeInsets.all(10.0),
-        child: Icon(
-          citys[index].icons[index],
-          color: Colors.white,
-          size: 20.0,
         ),
       ),
     );
