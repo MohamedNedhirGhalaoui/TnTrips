60c2ea70a60ed1e8b215bcb4db0e0ddc9f9d0967
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-08-01 14:29:21 +0100

    creation du class City qui represente la city (Tuins,sfax,Sousse, Monastire,...)

---
+++ lib/data/models/citys.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class Citys {
+  String name;
+  List<Icon> icons;
+  Color color;
+  Citys({required this.name, required this.icons, required this.color});
+}
