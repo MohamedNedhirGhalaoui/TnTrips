d87d5fa1f7278286d39184d6d021f2ad09d99958
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-08-01 18:59:11 +0100

    update du fichier dr projet

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -2,3 +2,7 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
+const Color HOTELS = Color(0xFFC02828);
+const Color RESTAURANTS = Color(0xFFC028BA);
+const Color CAFES = Color(0xFF28C080);
+const Color SPORTS = Color(0xFFC05D28);
