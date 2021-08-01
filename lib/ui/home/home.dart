86eac3c2afdeff7346e0ed179e50d286b876dafc
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-08-01 18:10:42 +0100

    personalisation du home page pour l'aparance des villes sous forme des ListView

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:tn_trips/common/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:tn_trips/data/models/city.dart';
+import 'package:tn_trips/data/services/Utils.dart';
 import 'package:tn_trips/ui/home/drawer/navigation_drawer_widget.dart';
 import 'package:tn_trips/ui/home/page/chats_page.dart';
 import 'package:tn_trips/ui/home/page/contact_us_page.dart';
@@ -11,6 +13,7 @@ import 'package:tn_trips/ui/home/page/user_page.dart';
 class Home extends StatelessWidget {
   final urlImage =
       "https://scontent.ftun10-1.fna.fbcdn.net/v/t1.6435-9/198764751_1634485790073693_2079059947597904212_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=3qxqOM63EagAX9OzeaX&tn=mJOlMso_Q5NB-1Io&_nc_ht=scontent.ftun10-1.fna&oh=2a31ffd747f1da35a21f932364453dc5&oe=6127D881";
+  List<City> citys = Utils.getCitys();
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +36,72 @@ class Home extends StatelessWidget {
       ),
       drawer: NavigationDrawerWidget(),
       bottomNavigationBar: buildBottomNavigationBar(context),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 10.0,
+              ),
+              child: Text(
+                "Choose your citiy",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: citys.length,
+                  itemBuilder: (BuildContext contexte, int index) {
+                    return Container(
+                      margin: EdgeInsets.all(20.0),
+                      height: 150.0,
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image.asset(
+                                "assets/images/citys/" +
+                                    citys[index].imageName +
+                                    ".jpg",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 0.0,
+                            left: 0.0,
+                            right: 0.0,
+                            child: Container(
+                              height: 120.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(20.0),
+                                  bottomRight: Radius.circular(20.0),
+                                ),
+                                gradient: LinearGradient(
+                                  begin: Alignment.bottomCenter,
+                                  end: Alignment.topCenter,
+                                  colors: [
+                                    Colors.black.withOpacity(0.7),
+                                    Colors.transparent
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  }),
+            )
+          ],
+        ),
+      ),
     );
   }
 
