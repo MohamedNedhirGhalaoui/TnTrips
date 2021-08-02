b0c642db5d7ffc93350495a8bcffbe921f5c4000
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-08-02 23:21:38 +0100

    Refactor code

---
+++ lib/ui/home/widget/main_app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tn_trips/common/constants.dart';
+
+class MainAppBar extends StatefulWidget implements PreferredSizeWidget {
+  MainAppBar({Key? key}) : super(key: key);
+
+  @override
+  _MainAppBarState createState() => _MainAppBarState();
+
+  @override
+  Size get preferredSize => new Size.fromHeight(80.0);
+}
+
+class _MainAppBarState extends State<MainAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      actions: [
+        Container(
+          margin: EdgeInsets.only(right: 10),
+          padding: EdgeInsets.all(10),
+          child: ClipOval(
+            child: Image.network(
+                "https://scontent.ftun10-1.fna.fbcdn.net/v/t1.6435-9/198764751_1634485790073693_2079059947597904212_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=3qxqOM63EagAX9OzeaX&tn=mJOlMso_Q5NB-1Io&_nc_ht=scontent.ftun10-1.fna&oh=2a31ffd747f1da35a21f932364453dc5&oe=6127D881"),
+          ),
+        )
+      ],
+      iconTheme: IconThemeData(color: kPrimaryColor),
+      centerTitle: true,
+      backgroundColor: Colors.transparent,
+      title: CircleAvatar(
+        backgroundImage: AssetImage("assets/images/trip.png"),
+        radius: 26.5,
+      ),
+      elevation: 0.0,
+    );
+  }
+}
