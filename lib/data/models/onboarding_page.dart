8cc68ccfceebdd57d17a170523e941afc8e79a74
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-08-04 00:07:46 +0100

    creation d'un class pour la modilisation d'une onboarding page

---
+++ lib/data/models/onboarding_page.dart
@@ -0,0 +1,8 @@
+class OnBorardingPage {
+  final String title;
+  final String description;
+  final String image;
+
+  OnBorardingPage(
+      {required this.title, required this.description, required this.image});
+}
