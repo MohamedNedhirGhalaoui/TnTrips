025ecb398e10858cb99282b15ed675ba0668b727
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-08-23 16:01:00 +0000

    creation d'une methode 'fromJson' pour generer un objet de type subServiceCategoryImage un utilisant la Map qui vient du firestore

--- lib/data/models/sub_service_category_image.dart
+++ lib/data/models/sub_service_category_image.dart
@@ -1,4 +1,18 @@
 class SubServiceCategoryImage {
   final String imageName;
   const SubServiceCategoryImage({required this.imageName});
+
+  factory SubServiceCategoryImage.fromJson(Map<String, dynamic> json) {
+    return SubServiceCategoryImage(imageName: json['imgName']);
+  }
+
+  static List<SubServiceCategoryImage> fromJsonArray(List<dynamic> jsonParts) {
+    List<SubServiceCategoryImage> gallery = [];
+
+    jsonParts.forEach((jsonData) {
+      gallery.add(SubServiceCategoryImage.fromJson(jsonData));
+    });
+
+    return gallery;
+  }
 }
