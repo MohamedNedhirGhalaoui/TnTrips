88390680e934e41453581631406d1353e011865a
CHEIKH BEY GHALAOUI MOHAMED NEDHIR <cheikh-bey-ghalaoui.mohamednedhir@enis.tn>
2021-08-25 17:14:46 +0000

    recuperation est l'affichage des l'ensembles des hotels

--- lib/ui/home/page/hotels_page.dart
+++ lib/ui/home/page/hotels_page.dart
@@ -1,20 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tn_trips/common/constants.dart';
+import 'package:tn_trips/data/models/city.dart';
+import 'package:tn_trips/data/models/sub_service_category.dart';
+import 'package:tn_trips/data/services/data_base_service.dart';
 import 'package:tn_trips/ui/home/widget/build-bottom_navigation_bar.dart';
+import 'package:tn_trips/ui/home/widget/build_icon.dart';
+import 'package:tn_trips/use_cases/category_selection.dart';
+
 class HotelsPage extends StatelessWidget {
+  List<SubServiceCategory> hotels = [];
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.local_hotel_rounded),
+  Widget build(BuildContext context) {
+    DataBaseservice dataBaseservice = Provider.of(context, listen: false);
+    List<City> citys = dataBaseservice.getCitys();
+    citys.forEach((city) {
+      city.servicecategorys[0].subServiceCategorys!
+          .forEach((subServiceCategory) {
+        hotels.add(subServiceCategory);
+      });
+    });
+
+    CategorySelection categorySelection =
+        Provider.of<CategorySelection>(context, listen: false);
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.local_hotel_rounded),
+          )
+        ],
+        title: Text('Hotels'),
+        centerTitle: true,
+        backgroundColor: kPrimaryColor,
+      ),
+      bottomNavigationBar: BuildBottomNavigationBar(),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                BuildIcon(color: HOTELS, icon: this.hotels[0].icon),
+                SizedBox(
+                  width: 10.0,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                physics: BouncingScrollPhysics(),
+                children: List.generate(
+                  this.hotels.length,
+                  (index) {
+                    return GestureDetector(
+                      onTap: () {
+                        categorySelection.selectedSubServiceCategory =
+                            this.hotels[index];
+                        Navigator.of(context).pushNamed('/detailesPage');
+                      },
+                      child: Container(
+                        child: Column(
+                          children: [
+                            ClipOval(
+                              child: Image.asset(
+                                "assets/images/services/hotels/tunisia/" +
+                                    this.hotels[index].imageName +
+                                    ".jpg",
+                                fit: BoxFit.cover,
+                                width: 150,
+                                height: 150,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              this.hotels[index].name,
+                              style: TextStyle(
+                                color: this.hotels[index].color,
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
             )
           ],
-          title: Text('Hotels'),
-          centerTitle: true,
-          backgroundColor: kPrimaryColor,
         ),
-        bottomNavigationBar: BuildBottomNavigationBar(),
-      );
+      ),
+    );
+  }
 }
